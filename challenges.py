3b6661f13244b20a5febedd343c5a1445d9965b3
matthewharrilal <matthewharrilal@gmail.com>
2017-10-24 09:04:15 -0700

    Completed a challenege on 10/23/17

---
+++ challenges.py
@@ -0,0 +1,35 @@
+import sys
+
+# #So essentially since we have to see if the sentence is a panagram or not first we have to get the imput of the user
+#
+# print("What is the sentence?")
+# sentence = input()
+# alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#
+# # So the task at hand is to check if the letters in the sentence containe each letter of the english alphabet
+# # for character in sentence:
+# #     for letter in alphabet:
+# #         if character == letter:
+# #             print("pangram")
+# #         else:
+# #             print("not pangram")
+# def is_pangram(sentence):
+#     for character in alphabet:
+#         if character in sentence:
+#             return("panagram")
+#         else:
+#             return("not panagram")
+#
+# print(is_pangram((sentence)))
+#
+#
+
+# Challenge 2
+# n = int(raw_input().strip())
+# s = raw_input().strip()
+# k = int(raw_input().strip())
+
+n
+
+def encrypt(encrypted_string):
+    pass
